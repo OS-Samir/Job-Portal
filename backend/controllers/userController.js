948355c3ee37e9fba1e54adde883fb9db6b20d6b
OS-Samir <samiradhikary972@gmail.comr>
2024-08-11 23:20:59 +0545

    changes in userController file

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,28 @@
-import {catchAsyncErrors} from "../";
+import {catchAsyncErrors} from "../middleware/catchAsyncErrors.js";
+import ErrorHandler from "../middleware/error.js";
+import {User} from "../models/userSchema.js"
+
+export const register = catchAsyncErrors(async(req, res, next) => {
+    try {
+        const {
+            name, 
+            email, 
+            phone, 
+            address, 
+            password, 
+            role, 
+            firstNiche, 
+            secondNiche, 
+            thirdNiche, coverLetter } = req.body;
+
+        if (!name || !email || !phone || !address || !password || !role){
+            return next(new ErrorHandler("All fields are required", 400))
+        }
+        if (role === "Job Seeker" && (!firstNiche || secondNiche || thirdNiche )) {
+            return next(new ErrorHandler("All fields are required", 400))
+
+        }
+    } catch (error) {
+
+    }
+})
