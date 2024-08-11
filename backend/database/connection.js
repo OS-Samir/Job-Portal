24fc211215cc67e7b1f5e362a96cb6e451857b52
OS-Samir <samiradhikary972@gmail.comr>
2024-08-11 20:25:47 +0545

    changes configure for port

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -5,7 +5,7 @@ export const connection = () => {
         dbName: "JOB_PORTAL_WITH_AUTOMATION"
     }).then(() => {
         console.log("connected to database");
-    }).catch(err=> {
-        console.log(`error connecting to database: ${err}`);
+    }).catch(error=> {
+        console.log(`error connecting to database: ${error}`);
     })
 }
