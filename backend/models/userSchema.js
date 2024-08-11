ed1408051b1d7ab6cd11dbe75465fed2d3e97aa7
OS-Samir <samiradhikary972@gmail.comr>
2024-08-11 22:57:17 +0545

    user schema defined

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -2,7 +2,55 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jwt";
 import validator from "validator";
+import { type } from "express/lib/response";
 
 const userSchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: true,
+        minLength: [3, "Name must contain at least 3 characters"],
+        maxLength: [30, "Name cannot exceed 30 characters"]
+
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: [validator.isEmail, "Please enter a valid email address"]
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    niches: {
+        firstNiche: String,
+        secondNiche: String,
+        thirdNiche: String
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: [8, "Password must contain at least 8 characters "],
+        maxLength: [30, "Password cannot exceed 30 characters"]
+    },
+    resume: {
+        public_id: String,
+        url: String
+    },
+    coverLetter: {
+        type: String
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["Job Seeker", "Employer"]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        
+    }
 });
