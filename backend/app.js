1e9ff6d6ccf0e0c4e492e564910fdca021a61e0d
OS-Samir <samiradhikary972@gmail.comr>
2024-08-12 22:14:34 +0545

    routers

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,9 @@ import { config } from "dotenv";// dotenv imported
 import cookieParser from "cookie-parser";
 import {connection} from "./database/connection.js";
 import { errorMiddleware } from "./middleware/error.js";
+import fileUpload from 'express-fileupload';
+import userRouter from "./routes/userRouter.js";
+
 
 
 const app = express(); // instance of express
@@ -20,6 +23,13 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use(
+    fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+})
+);
+app.use("/api/v1/user", userRouter);
 connection();
 
 app.use(errorMiddleware)
