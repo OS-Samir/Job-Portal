95b8ad15b8c3c2ff748921645e28d7075a2a4523
OS-Samir <samiradhikary972@gmail.comr>
2024-09-14 23:13:16 +0545

    desctructing objects in jobController

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -2,3 +2,9 @@ import {catchAsyncErrors} from "../middleware/catchAsyncErrors.js";
 import ErrorHandler from "../middleware/error.js";
 import {User} from "../models/userSchema.js";
 import { Job } from "../models/jobSchema.js";
+
+
+export const postJob = catchAsyncErrors(async (req, res, next) => {
+    const {title, jobType, location, companyName,  introduction, responsibilities, qualifications, offers,  salary, hiringMultipleCandidates,  personalWebsite, jobNiche, newsLetterSend   } = req.body;
+  
+})
