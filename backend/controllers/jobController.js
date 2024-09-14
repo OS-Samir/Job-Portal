3a94c47038278caddcb1bc18799ad7acf1794eea
OS-Samir <samiradhikary972@gmail.comr>
2024-09-14 23:07:01 +0545

    jobController created

---
+++ backend/controllers/jobController.js
@@ -0,0 +1,4 @@
+import {catchAsyncErrors} from "../middleware/catchAsyncErrors.js";
+import ErrorHandler from "../middleware/error.js";
+import {User} from "../models/userSchema.js";
+import { Job } from "../models/jobSchema.js";
