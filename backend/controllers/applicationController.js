b74f307a7e02131add25d69777cb358a018441a4
OS-Samir <samiradhikary972@gmail.comr>
2024-09-21 23:02:42 +0545

    post application function

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -2,7 +2,16 @@ import {catchAsyncErrors} from "../middleware/catchAsyncErrors.js";
 import ErrorHandler from "../middleware/error.js";
 import { Application } from "../models/applicationSchema.js";
 
-export const postApplication = catchAsyncErrors(async(req, res, next)=> {});
+export const postApplication = catchAsyncErrors(async(req, res, next)=> {
+    const {id} = req.params;
+    const {name, email, phone, address, coverLetter} = req.body;
+    if (!name || !email || !phone || !address || !coverLetter){
+         return next(new ErrorHandler("All fields must be filled", 400));
+    }
+    const jobSeekerInfo = {
+        
+    }
+});
 export const employerGetAllApplication = catchAsyncErrors(async(req, res, next)=> {});
 export const jobSeekerGetAllApplication = catchAsyncErrors(async(req, res, next)=> {});
 export const deleteApplication = catchAsyncErrors(async(req, res, next)=> {});
