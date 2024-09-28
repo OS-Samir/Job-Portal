3d63c58fa93fabe918d10dd9e90cdafeb775afc8
OS-Samir <samiradhikary972@gmail.comr>
2024-09-28 22:31:20 +0545

    working on automation functionality

--- backend/automation/newsLetterCron.js
+++ backend/automation/newsLetterCron.js
@@ -5,6 +5,20 @@ import { sendEmail } from '../utils/sendEmail.js';
 
 export const newLetterCron = () => {
 cron.schedule("*/1 * * * *", async () => {
+    const jobs = await Job.find({newsLettersSent: false});
+    for (const job of jobs) {
+        try {
+            const filteredUsers = await User.find({
+                $or: [
+                    {"niches: firstNiche": job.jobNiche},
+                    {"niches: secondNiche": job.jobNiche},
+                    {"niches: thirdNiche": job.jobNiche},
 
+                ]
+            })
+        } catch (error) {
+
+        }
+    }
 } )
 }
