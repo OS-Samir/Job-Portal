df3b860acbb4be332e8707f33252655c7f62bbc5
OS-Samir <samiradhikary972@gmail.comr>
2024-10-03 21:28:34 +0545

    frontend pages added and work to be done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+
 import "./App.css"
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Jobs from './pages/Jobs'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
+import Register from './pages/Register'
+import PostApplication from './pages/PostApplication'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-
+<Router>
+  <Navbar />
+  <Routes>
+    <Route path= "/" element={<Home />}/>
+    <Route path= "/jobs" element={<Jobs />}/>
+    <Route path= "/dashboard" element={<Dashboard />}/>
+    <Route path='/post/application/:jobId' element={<PostApplication />}/>
+    <Route path='/register' element={<Register />}/>
+    <Route path='/login' element={<Login />}/>
+    <Route path='*' element={<NotFound />}/>
+  </Routes>
+  <Footer />
+</Router>
     </>
   )
 }
