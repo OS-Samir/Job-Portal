b0d84f0d1beb509d1b3d8bb0b64b7aab2154aedc
OS-Samir <samiradhikary972@gmail.comr>
2024-10-05 21:59:18 +0545

    howitworks and hero pages completed

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+        <h1>Find your dream job today</h1>
+        <h4>Connecting talent with opportunities across the nation for every skill level</h4>
+        <div className='box'>
+        Explore a vast array of job listings in diverse industries. Whether
+        you're a seasoned professional or just starting out, find the perfect
+        role to advance your career. Our platform makes job searching easy and
+        efficient, bringing you closer to your next big opportunity.
+        </div>
+    </section>
+  )
+}
+
+export default Hero
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import Hero from '../components/Hero'
+import TopNiches from '../components/TopNiches'
+import HowItWorks from '../components/HowItWorks'
 const Home = () => {
   return (
-    <div>
-  <h1>This is homepage</h1>
-    </div>
+    <>
+   <Hero />
+   <TopNiches />
+   <HowItWorks />
+   </>
+
   )
 }
 
