20a066843e4369f1275382131ef8d8c3262fa665
OS-Samir <samiradhikary972@gmail.comr>
2024-10-18 16:36:45 +0545

    work on login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {useDispatch, useSelector} from "react-redux"
+import {useNavigate} from "react-router-dom";
 const Login = () => {
+  const [role, setRole] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const {loading, isAuthenticated, error} = useSelector(state => state.user)
+  const dispatch = useDispatch();
+  const navigateTo = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("role", role);
+    formData.append("email", email);
+    formData.append("password", password);
+  }
   return (
-    <div>
-      
-    </div>
+  <>
+  </>
   )
 }
 
