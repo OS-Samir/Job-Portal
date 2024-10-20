9af448f610ab271741f55652edade39a8cba731a
OS-Samir <samiradhikary972@gmail.comr>
2024-10-20 15:46:16 +0545

    nav bar completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
-
+import { Link } from 'react-router-dom';
+import {GiHamburgerMenu} from "react-icons/gi"
 const Navbar = () => {
   const [show, setShow] = useState(false);
   const {isAuthenticated} = useSelector(state => state.user);
@@ -10,11 +11,37 @@ const Navbar = () => {
       <div className="logo">
         <img src="/logo.png" alt="logo" />
         <h4>Job Fear</h4>
-        
       </div>
       <div className='links'>
-
-        </div>
+        <ul>
+          <li>
+            <Link to ={"/"} onClick={()=> setShow(!show)}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to ={"/jobs"} onClick={()=> setShow(!show)}>
+              Jobs
+            </Link>
+          </li>
+          {
+            isAuthenticated ? (
+              <li>
+              <Link to ={"/dashboard"} onClick={()=> setShow(!show)}>
+                Dashboard
+              </Link>
+            </li>
+            ) : (
+              <li>
+              <Link to ={"/login"} onClick={()=> setShow(!show)}>
+                Login
+              </Link>
+            </li>
+            )
+          }
+        </ul>
+      </div>
+      <GiHamburgerMenu className='hamburger' onClick={()=> setShow(!show)}/>
     </nav>
     </>
   )
