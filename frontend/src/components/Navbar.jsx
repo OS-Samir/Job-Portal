c16a57d87ac98fc05c6b13c0c8cc7f79d6193233
OS-Samir <samiradhikary972@gmail.comr>
2024-10-20 14:45:02 +0545

    designing in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const {isAuthenticated} = useSelector(state => state.user);
   return (
     <>
-    Navbar
+    <nav className={show ? "navbar show_navbar": "navbar"}>
+      <div className="logo">
+        <img src="/logo.png" alt="logo" />
+        <h4>Job Fear</h4>
+        
+      </div>
+      <div className='links'>
+
+        </div>
+    </nav>
     </>
   )
 }
