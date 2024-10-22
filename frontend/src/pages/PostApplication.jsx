68308f1ac7ef09ccd0bc3dc089fe74fd29f28b31
OS-Samir <samiradhikary972@gmail.comr>
2024-10-22 18:27:01 +0545

    application reducer imported in store

--- frontend/src/pages/PostApplication.jsx
+++ frontend/src/pages/PostApplication.jsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux'
 const PostApplication = () => {
   const {singleJob} = useSelector((state) => state.jobs)
   const {isAutheticated, user} = useSelector((state) => state.user)
+  
   return (
     <>
 
--- frontend/store/slices/applicationSlice.js
+++ frontend/store/slices/applicationSlice.js
@@ -1,6 +1,6 @@
+
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-
 const applicationSlice = createSlice({
     name: "applications",
     initialState: {
