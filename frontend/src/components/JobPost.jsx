5b3ddd1dc72307f72b107c8b8ae001bd17d196d5
OS-Samir <samiradhikary972@gmail.comr>
2024-10-31 22:00:44 +0545

    Post job page completed

--- frontend/src/components/JobPost.jsx
+++ frontend/src/components/JobPost.jsx
@@ -1,4 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { FaHandsAslInterpreting } from 'react-icons/fa6';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import {toast} from "react-toastify";
+import { clearAllJobErrors, postJob, resetJobSlice } from '../../store/slices/jobSlice';
+import {CiCircleInfo} from "react-icons/ci"
+
 
 const JobPost = () => {
   const [title, setTitle] = useState("");
@@ -107,11 +114,192 @@ const JobPost = () => {
     "Gorkha"
   ];
 
+  const {isAuthenticated, user} = useSelector((state) => state.user);
+  const {loading, error, message} = useSelector((state) => state.jobs);
+  const dispatch = useDispatch();
+
+  const handlePostJob = (e) => {
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("jobType", jobType);
+    formData.append("location", location);
+    formData.append("companyName", companyName);
+    formData.append("introduction", introduction);
+    formData.append("responsibilites", responsibilities);
+    formData.append("qualifications",qualifications);
+    offers && formData.append("offers", offers);
+    formData.append("jobNiche",jobNiche);
+    formData.append("salary", salary );
+    hiringMultipleCandidates && formData.append("hiringMultipleCandidates",hiringMultipleCandidates);
+    personalWebsiteTitle && formData.append("personalWebsiteTitle",personalWebsiteTitle);
+    personalWebsiteUrl && formData.append("personalWebsiteUrl",personalWebsiteUrl);
+
+  dispatch(postJob(formData))
+
+  };
+
+  useEffect(()=> {
+    if(error){
+      toast.error(error);
+      dispatch(clearAllJobErrors());
+    }
+    if(message){
+      toast.success(message);
+      dispatch(resetJobSlice());
+    }
+  }, [dispatch, error, message, loading])
+
+
+
   return (
-    <div>
+    <div className='account_components'>
+      <h3>Post a job</h3>
+      <div>
+        <label>Title</label>
+        <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} placeholder='Job Title' />
+      </div>
+
+      <div>
+        <label>Job Type</label>
+        <select  value={jobType} onChange={(e)=> setJobType(e.target.value)}>
+          <option value="">Select Job Type</option>
+          <option value="Full-time">Full-time</option>
+          <option value="Part-time">Part-time</option>
+
+        </select>
+      </div>
+
+      <div>
+        <label>Location(city)</label>
+        <select  value={location} onChange={(e)=> setLocation(e.target.value)}>
+        <option value="">Select Job Type</option>
+
+          {
+
+            cities.map((element)=> {
+              return (
+                  <option value={element}>{element}</option>
+              )
+            })
+
+
+          }
+
+        </select>
+      </div>
+
+      <div>
+        <label>Company Name</label>
+        <input type="text" value={companyName} onChange={(e)=>setCompanyName(e.target.value)} placeholder='Job Title' />
+      </div>
+
+      <div>
+        <label>Company/Job Introduction</label>
+          <textarea value={introduction} onChange={(e)=>setIntroduction(e.target.value)} placeholder='Company / Job Introduction' rows={10}/>
+      </div>
+
+      <div>
+        <label>Responsibilites</label>
+        <textarea value={responsibilities} onChange={(e)=>setResponsibilities(e.target.value)} placeholder='Job Responsibilites' rows={10}/>
+
+      </div>
+
+      <div>
+        <label>Qualifications</label>
+        <textarea value={qualifications} onChange={(e)=>setQualifications(e.target.value)} placeholder='Required Qualifications for job' rows={10}/>
+      </div>
+
+      <div>
+
+        <div className='label-infoTag-wrapper'> 
+        <label>What we Offer</label>
+          <span>
+          <CiCircleInfo /> Optional  
+        </span>
+        </div>
+       
+        <textarea value={offers} onChange={(e)=>setOffers(e.target.value)} placeholder='What are we offering!' rows={10}/>
+
+      </div>
+
+      <div>
+        <label>Job Niche</label>
+        <select  value={jobNiche} onChange={(e)=> setJobNiche(e.target.value)}>
+        <option value="">Select Job Niche</option>
+
+          {
+
+            niches.map((element)=> {
+              return (
+                  <option value={element}>{element}</option>
+              )
+            })
+
+
+          }
+
+        </select>
+      </div>
+
       
+      <div>
+        <label>Salary</label>
+        <input type="text" value={salary} onChange={(e)=>setSalary(e.target.value)} placeholder='40000 - 100000' />
+      </div>
+
+
+      <div>
+        
+        <div className='label-infoTag-wrapper'>
+        <label>Hiring Mutiple Candidates</label>
+        <span>
+          <CiCircleInfo /> Optional  
+        </span>
+        </div>
+       
+        <select  value={hiringMultipleCandidates} onChange={(e)=> setHiringMultipleCandidates(e.target.value)}>
+          <option value="">Hiring Multiple Candidates</option>
+          <option value="Yes">Yes</option>
+          <option value="No">No</option>
+
+        </select>
+
+      </div>
+
+
+      <div>
+        
+        <div className='label-infoTag-wrapper'>
+        <label>Personal Website Name </label>
+        <span>
+          <CiCircleInfo /> Optional  
+        </span>
+        </div>
+       
+          <input type="text"  value={personalWebsiteTitle} onChange={(e) => setPersonalWebsiteTitle(e.target.value)} placeholder='Personal Website Name/Title'/>
+
+      </div>
+
+      <div>
+        
+        <div className='label-infoTag-wrapper'>
+        <label>Personal Website URL Link </label>
+        <span>
+          <CiCircleInfo /> Optional  
+        </span>
+        </div>
+       
+          <input type="text"  value={personalWebsiteUrl} onChange={(e) => setPersonalWebsiteUrl(e.target.value)} placeholder='Personal Website URL Link'/>
+
+      </div>
+          <div>
+            <button style={{margin: "0 auto"}} className='btn' onClick={handlePostJob} disabled={loading}>
+              Post Job
+            </button>
+          </div>
+
     </div>
   )
 }
 
-export default JobPost
+export default JobPost;
